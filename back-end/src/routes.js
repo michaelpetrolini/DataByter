95ae4a3c1b8a2cae9f4feebd8fa13cd1c027692e
michaelpetrolini <54975151+michaelpetrolini@users.noreply.github.com>
2021-01-05 16:51:02 +0000

    Update routes.js
    be visualizzazione e aggiunta entries

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -63,37 +63,113 @@ function routes(app) {
     const mongoClient = require('mongodb').MongoClient;
     const url = "mongodb://localhost:27017/";
 
-    app.get('/tasks', (req, resp) => {
-        console.debug('Retrieving all tasks');
-
-        const objects = tasks.map(toDTO);
-        resp.json({
-            total: objects.length,
-            results: objects
+    app.get('/projects', (req, resp) => {
+        console.debug('Retrieving all projects');
+        mongoClient.connect(url, function(err, db) {
+            if (err)
+                throw err;
+            const dbo = db.db("databyter");
+            const getPromise = () => {
+                return new Promise((resolve, reject) => {
+                    dbo.collection("projects").find({}).toArray(function (err, result) {
+                        err ? reject(err) : resolve(result);
+                    });
+                });
+            };
+            const execGetPromise = async () => {
+          
+                var result = await (getPromise());
+                return result;
+             };
+             execGetPromise().then(function(result) {          
+                db.close();
+                resp.json({
+                    total: result.length,
+                    results: result
+                });
+             });
         });
     });
 
-    app.post('/task', (req, resp) => {
-        const {description} = req.body;
-        console.debug('Attempting to crete a new task', {description});
+    app.get('/project', (req, resp) => {
+        const projectId = parseInt(req.query.id);
+        console.debug('Retrieving project with id ' + projectId);
+        mongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            const dbo = db.db("databyter");
+            dbo.collection("projects").findOne({"projectId": projectId}, function(err, project) {
+                if (err) throw err;
+                db.close();
+                resp.json({
+                    project: project
+                });
+            });
+        });
+    });
 
-        if (!isNonBlank(description)) {
+    app.get('/entries', (req, resp) => {
+        console.debug('Retrieving project details');
+        mongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            const dbo = db.db("databyter");
+            const projectId = parseInt(req.query.id);
+            dbo.collection("projects").findOne({"projectId": projectId}, function(err, projectHeader) {
+                if (err) throw err;
+                const getPromise = () => {
+                    return new Promise((resolve, reject) => {
+                        dbo.collection("entries").find({"projectId": projectId}).toArray(function (err, entries) {
+                            err ? reject(err) : resolve(entries);
+                        });
+                    });
+                };
+                const execGetPromise = async () => {
+              
+                    var result = await (getPromise());
+                    return result;
+                 };
+                 execGetPromise().then(function(result) {          
+                    db.close();
+                    resp.json({
+                        header: projectHeader,
+                        total: result.length,
+                        results: result
+                    });
+                 });
+            });
+        });
+    });
+
+    app.post('/addEntry', (req, resp) => {
+        const newEntry = req.body;
+        const projectId = parseInt(req.query.id);
+        console.debug('Attempting to add a new entry', newEntry);
+        /*
+        if (!isNonBlank(newProject.pName) || newProject.fields.length === 0 || newProject.labels.length === 0) {
             resp.status(400);
-            resp.json({error: 'Missing task description'});
+            resp.json({error: 'Error in the format of the project'});
             return;
         }
-        if (description.trim().length > 50) {
-            resp.status(400);
-            resp.json({error: 'Too long task description'});
-            return;
-        }
-
-        const task = new Task(seq(), description.trim());
-        tasks.push(task);
-        console.info('Task successfully created', {task});
-
-        resp.status(201);
-        resp.json(toDTO(task));
+        */
+        mongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            const dbo = db.db("databyter");
+            const query = {projectId: projectId};
+            const today = new Date().toISOString().slice(0,10);
+            const update = {$set:{lastUpdate: today}, $inc:{entryId: 1}};
+            dbo.collection("projects").findOneAndUpdate(query, update, function(err, result) {
+              if (err) throw err;
+              newEntry.projectId = result.value.projectId;
+              newEntry.entryId = result.value.entryId;
+              newEntry.creationDate = today;
+              dbo.collection("entries").insertOne(newEntry, function(err, result) {
+                if (err) throw err;
+                console.log("Entry saved successfully");
+                db.close();
+                resp.status(201);
+                resp.json(newEntry);
+              });
+            });
+        });
     });
 
     app.post('/saveProject', (req, resp) => {
@@ -116,12 +192,12 @@ function routes(app) {
               dbo.collection("projects").insertOne(newProject, function(err, result) {
                 if (err) throw err;
                 console.log("Project saved successfully");
+                db.close();
+                resp.status(201);
+                resp.json(newProject);
               });
-              db.close();
             });
         });
-        resp.status(201);
-        resp.json(newProject);
     });
 
     app.put('/task/:id', (req, resp) => {
@@ -159,27 +235,40 @@ function routes(app) {
         resp.json(toDTO(task));
     });
 
-    app.delete('/task/:id', (req, resp) => {
-        const idRaw = req.params.id;
-        console.debug('Attempting to delete task', {id: idRaw});
+    app.delete('/entry', (req, resp) => {
+        const projectId = parseInt(req.query.projectId);
+        const entryId = parseInt(req.query.entryId);
+        console.debug('Attempting to delete entry ', {projectId: projectId, entryId: entryId});
+        mongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            const dbo = db.db("databyter");
+            const query = {projectId: projectId, entryId: entryId};
+            dbo.collection("entries").deleteOne(query, function(err, result) {
+                if (err) throw err;
+                console.info('Entry successfully deleted');
+                resp.status(200);
+                resp.json(toDTO(result));
+            });
+        });
+    });
 
-        if (!isInteger(idRaw)) {
-            resp.status(400);
-            resp.json({error: 'Invalid task identifier'});
-            return;
-        }
-        const id = parseInt(idRaw, 10);
-        const j = tasks.findIndex(t => t.id === id);
-        if (j < 0) {
-            resp.status(404);
-            resp.json({error: 'Task not found'});
-            return;
-        }
-        const [task] = tasks.splice(j, 1);
-
-        console.info('Task successfully deleted', {task});
-        resp.status(200);
-        resp.json(toDTO(task));
+    app.delete('/project', (req, resp) => {
+        const projectId = parseInt(req.query.projectId);
+        console.debug('Attempting to delete project ', {projectId: projectId});
+        mongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            const dbo = db.db("databyter");
+            const query = {projectId: projectId};
+            dbo.collection("projects").deleteOne(query, function(err, result) {
+                if (err) throw err;
+                dbo.collection("entries").deleteMany(query, function(err, result){
+                    console.info('Project successfully deleted');
+                    if (err) throw err;
+                    resp.status(200);
+                    resp.json(toDTO(result));
+                });
+            });
+        });
     });
 }
 
